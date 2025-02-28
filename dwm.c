35e6318ca9a279d8b2535d5737d0e4bc56d55a64
timoxxor <lepehintimofei2018@gmail.com>
2025-02-28 22:15:58 +0300

    added fullscreen fn crtl+shift+F

--- dwm.c
+++ dwm.c
@@ -226,6 +226,7 @@ static void tile(Monitor *m);
 static void togglebar(const Arg *arg);
 static void toggleextrabar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void togglewin(const Arg *arg);
@@ -1934,6 +1935,13 @@ togglefloating(const Arg *arg)
 		resize(selmon->sel, selmon->sel->x, selmon->sel->y,
 			selmon->sel->w, selmon->sel->h, 0);
 	arrange(selmon);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
